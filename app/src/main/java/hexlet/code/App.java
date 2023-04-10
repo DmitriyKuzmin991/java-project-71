85d197aea050090d4f963e52ee0e305d3e4cfcc0
DmitriySK <dmitrykuzminn@gmail.com>
2023-04-10 14:40:15 +0300

    add picocli, command -h and -V

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,32 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+@Command(version = "Differ version 0.1", header = "DifferHeader",
+        description = "Compares two configuration files and shows a difference.",
+        mixinStandardHelpOptions = true)
+public class App implements Runnable {
+
+    @Option(names = {"-h", "--help"}, usageHelp = true,
+            description = "Print usage help and exit.")
+    boolean usageHelpRequested;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true,
+            description = "Print version information and exit.")
+    boolean versionHelpRequested;
+
+    public void run() {
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Hello world");
+        }
+    }
+
+
+    public static void main(String... args) {
+        new CommandLine(new App()).execute(args);
     }
 }
+
+
